7660b6c46641a8bfdecea5053ecf58acae7be0e8
Praveenkusuluri08 <Praveenkusuluri07@gmail.com>
2022-01-04 21:28:41 +0530

    Get authenticated user data likes of the user and posts by the user

--- functions/Services/User/model.js
+++ functions/Services/User/model.js
@@ -1,3 +1,4 @@
+const { data } = require("autoprefixer");
 const { reduceUserDetails } = require("../../helpers/utils");
 const { admin, db } = require("../../utils/admin");
 const AuthUtils = require("../Authentication/utils");
@@ -9,14 +10,55 @@ class User {
   async updateUserProfile(inputData, userId) {
     const updateData = {
       name: inputData.name,
-      
-
+      lastUpdate: new Date().toISOString(),
     };
     console.log(this.actionPerformer);
     let userDetails = reduceUserDetails(inputData);
     return AuthUtils._userExists(userId)
       .then(() => {
-        db.collection("USERS").doc(userId).update(userDetails);
+        db.collection("USERS").doc(userId).update(updateData);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+  /** 
+  @getAuthUserData 
+      this function is mainly useful for to get the loggedIn userData
+      data which we need to recieve is personal userData, likes, posts
+  */
+  async getAuthUserData() {
+    let user = {};
+    return db
+      .collection("USERS")
+      .doc(this.actionPerformer.uid)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          user["creds"] = doc.data();
+          return db
+            .collection("LIKES")
+            .where("email", "==", this.actionPerformer.email)
+            .get();
+        }
+      })
+      .then((data) => {
+        user["likes"] = [];
+        data.forEach((like) => {
+          user["likes"].push(like.data());
+        });
+        return db
+          .collection("POSTS")
+          .where("email", "==", this.actionPerformer.email)
+          .get();
+      })
+      .then((postsData) => {
+        user["posts"] = [];
+
+        postsData.forEach((post) => {
+          user["posts"].push(post.data());
+        });
+        return user;
       })
       .catch((err) => {
         throw err;
